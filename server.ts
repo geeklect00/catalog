63250b94bc48a3eb49a6719d82f78fd60b4b6ddb
geeklect00 <canxakgun@gmail.com>
2026-03-01 07:00:52 +0300

    PORT environment variable eklendi

--- server.ts
+++ server.ts
@@ -398,10 +398,7 @@ async function startServer() {
     });
   }
 
-  const PORT = 3000;
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
-startServer();
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
